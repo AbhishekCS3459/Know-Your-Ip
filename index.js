6c400414ea8964caacd414f8b0d09fcd0b3103d6
AbhishekCS3459 <abhishekverman3459@gmail.com>
2024-03-10 12:49:12 +0530

    changing the ip deps

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import express from "express";
+import dotenv from "dotenv";
+import IP from 'ip'
+const PORT=process.env.PORT||4000;
 
 const app=express();
 app.get("/",(req,res)=>{
-    const IP=req.socket.remoteAddress
-    res.send(`Your IP address is ${IP}`);
+    const ipaddress=IP.address();
+    res.send(`Your IP address is ${ipaddress}`);
 })
-app.listen(3000,()=>{
+app.listen(PORT,()=>{
     console.log("Server is running on port 3000");
 })
